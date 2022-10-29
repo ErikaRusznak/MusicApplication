1933d98a01e21eef0724f2ee75d6dfb8dd26b89c
ErikaRusznak <erika.rusznak@gmail.com>
2022-10-29 13:06:52 +0300

    finished about, prices and contact pages

--- client/src/component/StartingPage/StartingPage.js
+++ client/src/component/StartingPage/StartingPage.js
@@ -2,7 +2,6 @@ import "./StartingPage.css";
 import { Background } from "./images";
 
 function StartingPage() {
-
   return (
     <div className="body">
       <nav class="startNavbar">
@@ -14,7 +13,7 @@ function StartingPage() {
               <a href="/about">About</a>
             </li>
             <li>
-              <a href="/prices">Prices</a>
+              <a href="/prices">Buying Method</a>
             </li>
             <li>
               <a href="/contact">Contact</a>
@@ -30,9 +29,7 @@ function StartingPage() {
         <div className="welcomeMessage" style={{ height: "50vh" }}>
           <div className="col1">
             <h1>Welcome!</h1>
-            <p>
-              Thank you for choosing this application so you can buy music.
-            </p>
+            <p>Thank you for choosing this application so you can buy music.</p>
             <div className="d-flex justify-content-center">
               <button className="buttonsStartPage">
                 <a href="/login">Login</a>
